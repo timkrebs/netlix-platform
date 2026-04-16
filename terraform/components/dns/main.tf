ee7ad775085b561ed85bd94bee81432529dd40c2
Tim Krebs <48254824+timkrebs@users.noreply.github.com>
2026-04-16 10:11:55 +0200

    fix: update Route53 record resource to use correct domain validation options

--- terraform/components/dns/main.tf
+++ terraform/components/dns/main.tf
@@ -25,15 +25,19 @@ resource "aws_acm_certificate" "wildcard" {
 resource "aws_route53_record" "cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.wildcard.domain_validation_options :
-    dvo.resource_record_name => dvo...
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
   }
 
   allow_overwrite = true
   zone_id         = data.aws_route53_zone.main.zone_id
-  name            = each.value[0].resource_record_name
-  type            = each.value[0].resource_record_type
+  name            = each.value.name
+  type            = each.value.type
   ttl             = 60
-  records         = [each.value[0].resource_record_value]
+  records         = [each.value.record]
 }
 
 # ACM Certificate Validation
